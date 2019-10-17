26d6bddc971b2c2b072a0c69db1d0a562777af2f
rdogm13 <drg6525@yahoo.com>
2019-10-16 21:31:31 -0700

    form fields and button built with onsubmit for login

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import SignInForm from './signinForm';
 
 export default class SignIn extends Component {
-  render() {
-      return (
-          <div className="sign-in">
-              <SignInForm className='sign-in__form' />
-          </div>
-      )
-  }
+
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                <SignInForm className='sign-in__form' onSubmit={this.onSubmit} />
+            </div>
+        )
+    }
 
 }
