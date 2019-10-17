0fd8b58254bd739c226421de3903d4b7720be9f0
rdogm13 <drg6525@yahoo.com>
2019-10-16 21:07:29 -0700

    built form input components

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
+import SignInForm from './signinForm';
 
 export default class SignIn extends Component {
-  constructor(props) {
-      super(props)
-
-      this.state = {
-          
-      }
-  }
-
   render() {
       return (
           <div className="sign-in">
-              sign in
+              <SignInForm className='sign-in__form' />
           </div>
       )
   }
-
-  SignIn = reduxForm({
-      form: 'SignIn'
-  })(SignIn);
 
 }
---
+++ src/components/auth/signinForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { FormInput } from '../formFields';
+
+class SignInForm extends Component {
+    render() {
+        const { className } = this.props;
+        return (
+            <form className={`${className} sign-in-form`}>
+                <Field
+                    className='sign-in-form__email'
+                    type='email'
+                    title='E-mail'
+                    placeholder='email'
+                    name='email'
+                    component={FormInput} />
+                <Field
+                    className='sign-in-form__password'
+                    type='password'
+                    title='Password'
+                    placeholder='password'
+                    name='password'
+                    component={FormInput} />
+            </form>
+        )
+    }
+
+
+}
+
+SignInForm = reduxForm({
+    form: 'SignInForm'
+})(SignInForm);
+
+export default SignInForm;
