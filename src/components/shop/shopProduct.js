2dda30b182668122a77385b1e58fcd2a8225384c
rdogm13 <drg6525@yahoo.com>
2019-10-23 08:54:42 -0700

    add to cart functionality

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Quantity from '../quantity';
 import GreenPriceTag from '../greenPriceTag';
 
+import * as actions from '../../actions';
+import { connect } from 'react-redux';
+
 
 
 
@@ -10,6 +13,8 @@ class ShopProduct extends Component {
     handleAddToCart = () => {
         if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
             document.getElementById('shop-cart').classList.remove('cart-hidden');
+            const { _id, title, description, price, belongsTo } = this.props;
+            this.props.addCartProduct({ _id, title, description, price, belongsTo });
         } else {
             document.getElementById('shop-cart').classList.add('cart-hidden');
         }
@@ -42,5 +47,7 @@ class ShopProduct extends Component {
         )
     }
 }
+
+ShopProduct = connect(null, actions)(ShopProduct);
 
 export default ShopProduct
